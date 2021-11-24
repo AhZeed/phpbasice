1edf54767283a8ad39839984f3e10ea0788836cb
AhZeed <kingkua204@gmail.com>
2021-11-24 20:57:36 +0700

    test1
    haha to ez

---
+++ varible.php
@@ -0,0 +1,49 @@
+<?php
+    print("hello");
+    print("hello");
+    print("hello"); 
+    print("<br>");
+    print("hello");
+    $name="kua";
+    $age="20";
+    $relation=true;
+    echo($name);
+    define("provinee","bangkok");
+    echo ("provice");
+    echo("<br>");
+    define("provine","15000");
+    echo ("salary");
+    echo("<br>");
+    $a= 1;
+    $b=1;
+    if($a === $b){
+        echo("true");
+    }else{
+        echo("False");
+    }
+   
+    $t = date("H");
+
+    if ($t < "10") {
+    echo "Have a good morning!";
+    } elseif ($t < "20") {
+     echo "Have a good day!";
+    } else {
+     echo "Have a good night!";
+}
+?>  
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     print("<h1>hello</h1>");
+    ?>
+</body>
+</html>
---
+++ variblework.php
@@ -0,0 +1,23 @@
+<?php
+
+$t = date("H");
+if ($t < "16") {
+    echo "Have a good afternoon";
+    } elseif ($t < "20") {
+     echo "Have a good day!";
+    } else {
+     echo "Have a good night!";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>variblework</title>
+</head>
+<body>
+    
+</body>
+</html>
