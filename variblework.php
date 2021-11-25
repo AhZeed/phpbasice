7f74286235e82f54c222bdfc9618058e3f12a705
AhZeed <kingkua204@gmail.com>
2021-11-25 19:54:01 +0700

    HomeWork

--- variblework.php
+++ variblework.php
@@ -8,6 +8,28 @@ if ($t < "16") {
     } else {
      echo "Have a good night!";
 }
+echo("<br>");
+$money = 80;
+
+if ($money >= 100) {
+    echo "ซื้อหนังสือการ์ตูน.";
+} else {
+    echo "ซื้อขนม.";
+}
+echo("<br>");
+$score = 79;
+
+if ($score >= 80) {
+    echo "Your grade is 4.";    
+} else if ($score >= 70) {
+    echo "Your grade is 3.";    
+} else if ($score >= 60) {
+    echo "Your grade is 2.";    
+} else if ($score >= 50) {
+    echo "Your grade is 1.";    
+} else {
+    echo "Sorry, you got grade 0."; 
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
